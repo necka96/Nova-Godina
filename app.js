14b497dd193dba74ea1e58dda88a42c19a511a15
Necka96 <nemanja2014@live.com>
2023-12-31 19:09:33 +0100

    change year

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-$('body').fireworks();
+$("body").fireworks();
+
+// old year & new year
+
+const oldYear = document.querySelector(".old_year");
+const newYears = document.querySelectorAll(".new_year");
+const currentYear = new Date().getFullYear();
+
+oldYear.textContent = currentYear;
+newYears.forEach((newYear) => (newYear.textContent = currentYear + 1));
